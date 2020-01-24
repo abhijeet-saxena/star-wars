15650125c22256dbdc171aa84f725433cd096096
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-01-25 00:00:00 +0530

    feat(app): Refactor main.js file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
-function prepareCrawl(event) {
+const stars = document.querySelector(".stars");
+
+const prepareCrawl = (event) => {
   event.preventDefault();
   const formData = new FormData(document.querySelector("form"));
   for (var [key, value] of formData.entries()) {
@@ -12,4 +14,17 @@ function prepareCrawl(event) {
   }
   document.querySelector(".prepare-crawl").style.display = "none";
   window.location.href = "./showcase";
-}
+};
+
+const generateStarBackground = () => {
+  const starsCount = 100;
+  let starsHTML = "";
+  for (let i = 0; i < starsCount; i++) {
+    let randomX = Math.floor(Math.random() * window.innerWidth);
+    let randomY = Math.floor(Math.random() * window.innerHeight);
+    starsHTML += `<span class="star" style="top: ${randomY}px; left: ${randomX}px"></span>`;
+  }
+  stars.innerHTML = starsHTML;
+};
+
+generateStarBackground();
