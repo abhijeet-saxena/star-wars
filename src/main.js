e8a470ba8ae48607155a88b4ab4ba60d511e392a
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-03-31 02:35:38 +0530

    feat(client): Adding main animations

--- src/main.js
+++ src/main.js
@@ -1,1 +1,44 @@
-alert("Once uppon a time in a galaxy long ago");
+const prologue = document.querySelector(".prologue");
+const banner = document.querySelector(".banner");
+const crawl = document.querySelector(".crawl-text");
+const starsCount = 50;
+
+window.onload = () => {
+  let scroll = 0;
+  //   let i = null;
+
+  let id = setInterval(() => {
+    crawl.scroll({
+      top: scroll,
+      behavior: "smooth"
+    });
+    scroll++;
+    if (1000 < scroll) {
+      clearInterval(id);
+      console.log(crawl.getBoundingClientRect());
+      console.log(scroll);
+    }
+  }, 16);
+
+  //   prologue.style.animation = "fade-out 5000ms ease-in forwards";
+  //   prologue.onanimationend = () => {
+  //     prologue.style.display = "none";
+  //     setTimeout(() => {
+  //       banner.style.display = "block";
+  //       generateStarBackground();
+  //     }, 200);
+  //   };
+};
+
+const generateStarBackground = () => {
+  let height = window.innerHeight;
+  let width = window.innerWidth;
+  let starsHTML = "";
+
+  for (let i = 0; i < starsCount; i++) {
+    let randomX = Math.floor(Math.random() * width);
+    let randomY = Math.floor(Math.random() * height);
+    starsHTML += `<span class="star" style="top: ${randomY}px; left: ${randomX}px"></span>`;
+  }
+  document.body.innerHTML += starsHTML;
+};
