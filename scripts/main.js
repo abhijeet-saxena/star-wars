03a9ad92dd03e3aeefec6b3b924926d3c640ee71
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-04-05 17:41:50 +0530

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+function prepareCrawl(event) {
+  event.preventDefault();
+  const formData = new FormData(document.querySelector("form"));
+  for (var [key, value] of formData.entries()) {
+    let HTML = "";
+    let bannerLines = value.trim().split("\n");
+
+    bannerLines.forEach((element) => {
+      if (element) HTML += `<p>${element}</p>`;
+    });
+    sessionStorage.setItem(key, HTML);
+  }
+  document.querySelector(".prepare-crawl").style.display = "none";
+  window.location.href = "./showcase.html";
+}
